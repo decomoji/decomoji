6392128ce6d73e7b89f86bdfd4c850efca6734b1
oti <otiext@gmail.com>
2018-06-13 23:53:38 +0900

    Add generate script of decomoji list markdown file

---
+++ scripts/generate_decomoji_list.js
@@ -0,0 +1,29 @@
+'use strict'
+
+let fs = require('fs')
+
+const header = {
+  basic: `# デコモジ基本セット\n\nすぐに使えてSlackが楽しくなる基本セットです。\n\n`,
+  extra: `# デコモジ拡張セット\n\n作りたいと思った気持ちのままに作った拡張セットです。使い方にご注意ください。\n\n`
+}
+
+
+function generate_decomoji_list(type) {
+  fs.readdir(`./decomoji/${type}/`, (error, files) => {
+    if (error) {
+      throw error
+    }
+
+    let text = ''
+
+    files.forEach((file) => {
+      if (file === '.DS_Store') return
+      text += `![${file.split('.')[0]}](./decomoji/${type}/${file})`
+    })
+
+    fs.writeFile(`./decomoji-${type}.md`, header[type]+text)
+  })
+}
+
+generate_decomoji_list('basic')
+generate_decomoji_list('extra')
