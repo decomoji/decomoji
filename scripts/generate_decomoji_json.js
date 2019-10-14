db9b29be5a188680c7850d003f46fe002e040438
oti <otiext@gmail.com>
2019-10-14 19:03:09 +0900

    Add build json task

---
+++ scripts/generate_decomoji_json.js
@@ -0,0 +1,29 @@
+'use strict'
+
+let fs = require('fs')
+
+function generate_decomoji_json(type) {
+  fs.readdir(`./decomoji/${type}/`, (err, files) => {
+    if (err) throw err
+
+    const decomoji_array =  files.reduce((memo, file) => {
+      return file === '.DS_Store'
+        ? memo
+        : memo.concat(file.split('.')[0])
+    }, [])
+
+    if (!fs.existsSync('./json')) fs.mkdirSync('./json')
+
+    fs.writeFile(
+      `./json/${type}.json`,
+      `{"${type}":`+JSON.stringify(decomoji_array)+'}',
+      (err) => {
+        if (err) throw err
+        console.log(`${type}.json has been saved!`)
+      }
+    )
+  })
+}
+
+generate_decomoji_json('basic')
+generate_decomoji_json('extra')
