8d50c16fb196a52ee251ccb46f280dff233b7860
oti <otiext@gmail.com>
2020-02-18 19:35:47 +0900

    [remove-script] remover.rb に引数を2つ渡して dir と json のモードを変えられるようにした

--- scripts/remove.rb
+++ scripts/remove.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'mechanize'
 
 require './remover'
-
-remover = Remover.new(remove_img_dir: ARGV[0])
+remover = Remover.new(remove_img_target: ARGV[0], target_mode: ARGV[1])
 remover.serial
 puts 'Done!'
