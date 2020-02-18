3895197cf7facf440b9913815000dba2c0cbf6d1
oti <otiext@gmail.com>
2020-02-18 19:40:59 +0900

    [remove-script] サンプルコードをメモした

--- scripts/remove.rb
+++ scripts/remove.rb
@@ -2,6 +2,12 @@ require 'highline/import'
 require 'json'
 require 'mechanize'
 
+# sample command
+#    dir: bundle exec ruby remove.rb basic
+#    dir: bundle exec ruby remove.rb extra
+#   json: bundle exec ruby remove.rb list_v4 json
+#   json: bundle exec ruby remove.rb list_v4-basic json
+
 require './remover'
 remover = Remover.new(remove_img_target: ARGV[0], target_mode: ARGV[1])
 remover.serial
