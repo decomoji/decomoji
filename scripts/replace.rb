5dfba7fd3eb7a8ee2fc785f407fb4fb2759c4845
oti <otiext@gmail.com>
2020-02-25 17:41:14 +0900

    [replace-script] ファイルリストとディレクトリを指定して、削除と追加を一括実行する replacer.rb を追加した

---
+++ scripts/replace.rb
@@ -0,0 +1,11 @@
+require 'highline/import'
+require 'json'
+require 'mechanize'
+
+# sample command
+#   - bundle exec ruby replacer.rb list_v4-basic.json basic
+
+require './replacer'
+replacer = Replacer.new(remove_target: ARGV[0], import_target: ARGV[1])
+replacer.serial
+puts 'Done!'
