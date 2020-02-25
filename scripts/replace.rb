17a54663b4b5510d64e8e39c44a3ac5a392c1152
oti <otiext@gmail.com>
2020-02-25 18:55:56 +0900

    [replace-script] ログインのためのアカウント情報をローカルの json から読み取れるようにした

--- scripts/replace.rb
+++ scripts/replace.rb
@@ -6,6 +6,6 @@ require 'mechanize'
 #   - bundle exec ruby replacer.rb list_v4-basic.json basic
 
 require './replacer'
-replacer = Replacer.new(remove_target: ARGV[0], import_target: ARGV[1])
+replacer = Replacer.new(remove_target: ARGV[0], import_target: ARGV[1], account: ARGV[2])
 replacer.serial
 puts 'Done!'
