9964d65428ed383659d79d748f5e3a6c31bfa9c0
oti <otiext@gmail.com>
2020-03-01 00:53:07 +0900

    [replace-script] サンプルコマンドを修正した

--- scripts/replace.rb
+++ scripts/replace.rb
@@ -3,7 +3,7 @@ require 'json'
 require 'mechanize'
 
 # sample command
-#   - bundle exec ruby replacer.rb list_v4-basic.json basic
+#   - bundle exec ruby replace.rb list_v4-basic.json basic
 
 require './replacer'
 replacer = Replacer.new(remove_target: ARGV[0], import_target: ARGV[1], account: ARGV[2])
