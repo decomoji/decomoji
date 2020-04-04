f96884b4508be30e5a99d30f2d40c9c9b97e2e61
Yoshiaki Ito <ito@maboroshi.biz>
2020-04-04 23:35:21 +0900

    [import-script] import スクリプトのサンプルコマンドを修正した

--- scripts/import.rb
+++ scripts/import.rb
@@ -3,8 +3,8 @@ require 'json'
 require 'mechanize'
 
 # sample command
-#   - bundle exec ruby import.rb preview account.json
-#   - bundle exec ruby import.rb alias-standard.json account.json
+#   - bundle exec ruby import.rb preview dir account.json
+#   - bundle exec ruby import.rb alias-standard.json alias account.json
 
 require './importer'
 importer = Importer.new(import_target: ARGV[0], import_mode: ARGV[1], account: ARGV[2])
