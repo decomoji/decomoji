6e7f814a65f9333dc759ea24fefbcf2362a27a52
geckotang <macky1207@gmail.com>
2020-05-12 15:22:01 +0900

    feat(importer): コマンドライン引数をCommanderで処理する

--- scripts/importer/index.js
+++ scripts/importer/index.js
@@ -1,3 +1,4 @@
+const program = require("commander");
 const puppeteer = require("puppeteer");
 
 const isStringOfNotEmpty = require("./utilities/isStringOfNotEmpty");
@@ -8,20 +9,16 @@ const getTargetDecomojiList = require("./modules/getTargetDecomojiList");
 const goToEmojiPage = require("./modules/goToEmojiPage");
 const importer = require("./modules/importer");
 
-// オプションをパースする
-const options = {};
-((argv) => {
-  argv.forEach((v, i) => {
-    const opt = v.split("=");
-    const key = opt[0].replace("--", "");
-    options[key] = opt.length > 1 ? opt[1] : true;
-  });
-})(process.argv);
+// コマンドライン引数の定義
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-i, --inputs', 'input setting json file')
+  .parse(process.argv);
 
 // 自動処理を実行する
 const main = async (inputs) => {
   // puppeteer でブラウザを起動する
-  const browser = await puppeteer.launch({ devtools: options.debug });
+  const browser = await puppeteer.launch({ devtools: program.debug });
   // ページを追加する
   const page = await browser.newPage();
 
@@ -30,24 +27,24 @@ const main = async (inputs) => {
   );
 
   // カスタム絵文字管理画面へ遷移する
-  inputs = await goToEmojiPage(page, inputs, options);
+  inputs = await goToEmojiPage(page, inputs, program);
 
   // 登録済みのカスタム絵文字リストを取得する
   const emojiAdminList = await page.evaluate(
     getEmojiAdminList,
     inputs.team_name
   );
-  options.debug &&
+  program.debug &&
     console.log("emojiAdminList:", emojiAdminList.length, emojiAdminList);
 
   // 対象デコモジリストを取得する
   const allDecomojiList = await getTargetDecomojiList(inputs.categories);
-  options.debug &&
+  program.debug &&
     console.log("allDecomojiList:", allDecomojiList.length, allDecomojiList);
 
   // emojiAdminList からファイル名だけの配列を作っておく
   const emojiAdminNameList = new Set(emojiAdminList.map((v) => v.name));
-  options.debug && console.log("emojiAdminNameList:", emojiAdminNameList);
+  program.debug && console.log("emojiAdminNameList:", emojiAdminNameList);
 
   // emojiAdminList と allDecomojiList を突合させて処理するアイテムだけのリストを作る
   const targetDecomojiList = allDecomojiList.map((category) =>
@@ -55,7 +52,7 @@ const main = async (inputs) => {
       (candidate) => !emojiAdminNameList.has(candidate.split(".")[0])
     )
   );
-  options.debug &&
+  program.debug &&
     console.log(
       "targetDecomojiList:",
       targetDecomojiList.length,
@@ -66,17 +63,17 @@ const main = async (inputs) => {
   await importer(page, inputs, targetDecomojiList);
 
   // 処理が終わったらブラウザを閉じる
-  if (!options.debug) {
+  if (!program.debug) {
     await browser.close();
   }
 };
 
-if (options.inputs) {
+if (program.inputs) {
   // --inputs=./something.json などと値が指定されていたらそれを require し
   // --inputs キーのみの場合はデフォルトで `./inputs.json` を require する
   main(
-    require(isStringOfNotEmpty(options.inputs)
-      ? options.inputs
+    require(isStringOfNotEmpty(program.inputs)
+      ? program.inputs
       : "./inputs.json")
   );
 } else {
