24f7a2e6854541c1e9931c75a0387a7028068b63
oti <otiext@gmail.com>
2020-05-12 18:33:04 +0900

    feat(importer): ログインから emoji.add へのリクエストループを再帰処理に変更した

--- scripts/manager/index.js
+++ scripts/manager/index.js
@@ -1,71 +1,20 @@
 const program = require("commander");
-const puppeteer = require("puppeteer");
 
 const isStringOfNotEmpty = require("./utilities/isStringOfNotEmpty");
 
 const askInputs = require("./modules/askInputs");
-const getEmojiAdminList = require("./modules/getEmojiAdminList");
-const getTargetDecomojiList = require("./modules/getTargetDecomojiList");
-const goToEmojiPage = require("./modules/goToEmojiPage");
 const importer = require("./modules/importer");
 
 // コマンドライン引数の定義
 program
-  .option('-d, --debug', 'output extra debugging')
-  .option('-i, --inputs', 'input setting json file')
+  .option("-d, --debug", "output extra debugging")
+  .option("-i, --inputs", "input setting json file")
   .parse(process.argv);
 
 // 自動処理を実行する
 const main = async (inputs) => {
-  // puppeteer でブラウザを起動する
-  const browser = await puppeteer.launch({ devtools: program.debug });
-  // ページを追加する
-  const page = await browser.newPage();
-
-  console.log(
-    `\nworkspace: https://${inputs.team_name}.slack.com/\n    email: ${inputs.email}\n password: **********\n\nConnecting...\n`
-  );
-
-  // カスタム絵文字管理画面へ遷移する
-  inputs = await goToEmojiPage(page, inputs, program);
-
-  // 登録済みのカスタム絵文字リストを取得する
-  const emojiAdminList = await page.evaluate(
-    getEmojiAdminList,
-    inputs.team_name
-  );
-  program.debug &&
-    console.log("emojiAdminList:", emojiAdminList.length, emojiAdminList);
-
-  // 対象デコモジリストを取得する
-  const allDecomojiList = await getTargetDecomojiList(inputs.categories);
-  program.debug &&
-    console.log("allDecomojiList:", allDecomojiList.length, allDecomojiList);
-
-  // emojiAdminList からファイル名だけの配列を作っておく
-  const emojiAdminNameList = new Set(emojiAdminList.map((v) => v.name));
-  program.debug && console.log("emojiAdminNameList:", emojiAdminNameList);
-
-  // emojiAdminList と allDecomojiList を突合させて処理するアイテムだけのリストを作る
-  const targetDecomojiList = allDecomojiList.map((category) =>
-    category.filter(
-      (candidate) => !emojiAdminNameList.has(candidate.split(".")[0])
-    )
-  );
-  program.debug &&
-    console.log(
-      "targetDecomojiList:",
-      targetDecomojiList.length,
-      targetDecomojiList
-    );
-
   // ファイルをアップロードする
-  await importer(page, inputs, targetDecomojiList);
-
-  // 処理が終わったらブラウザを閉じる
-  if (!program.debug) {
-    await browser.close();
-  }
+  await importer(inputs, program);
 };
 
 if (program.inputs) {
--- scripts/manager/modules/importer.js
+++ scripts/manager/modules/importer.js
@@ -1,40 +1,107 @@
-const postEmojiAdd = require("./postEmojiAdd");
+const puppeteer = require("puppeteer");
+
+const getEmojiAdminList = require("./getEmojiAdminList");
+const getTargetDecomojiList = require("./getTargetDecomojiList");
+const goToEmojiPage = require("./goToEmojiPage");
 const injectUploadForm = require("./injectUploadForm");
+const postEmojiAdd = require("./postEmojiAdd");
 
-const importer = async (page, inputs, targets) => {
-  await page.evaluate(injectUploadForm);
+const importer = async (inputs, program) => {
 
-  for(let i=0; i<targets.length; i++) {
-    const targetAsCategory = targets[i];
-    const amountAsCategory = targetAsCategory.length;
-    const targetCategoryName = inputs.categories[i];
-
-    console.log(`\n[${targetCategoryName}] category start!`)
-
-    let j = 0;
-    while(j < amountAsCategory) {
-      const item = targetAsCategory[j];
-      const targetBasename = item.split(".")[0];
+  const _import = async(inputs, program) => {
+    // puppeteer でブラウザを起動する
+    const browser = await puppeteer.launch({ devtools: program.debug });
+    // ページを追加する
+    const page = await browser.newPage();
   
-      console.log(`${j + 1}/${amountAsCategory}: importing ${targetBasename}...`);
+    console.log(
+      `\nworkspace: https://${inputs.team_name}.slack.com/\n    email: ${inputs.email}\n password: **********\n\nConnecting...\n`
+    );
   
-      const result = await postEmojiAdd(page, inputs.team_name, targetCategoryName, targetBasename, item);
+    // カスタム絵文字管理画面へ遷移する
+    inputs = await goToEmojiPage(page, inputs, program);
+
+    // 登録済みのカスタム絵文字リストを取得する
+    const emojiAdminList = await page.evaluate(
+      getEmojiAdminList,
+      inputs.team_name
+    );
+    // program.debug &&
+    //   console.log("emojiAdminList:", emojiAdminList.length, emojiAdminList);
+
+    // 対象デコモジリストを取得する
+    const allDecomojiList = await getTargetDecomojiList(inputs.categories);
+    // program.debug &&
+    //   console.log("allDecomojiList:", allDecomojiList.length, allDecomojiList);
+
+    // emojiAdminList からファイル名だけの配列を作っておく
+    const emojiAdminNameList = new Set(emojiAdminList.map((v) => v.name));
+    // program.debug && console.log("emojiAdminNameList:", emojiAdminNameList); 
+
+    // emojiAdminList と allDecomojiList を突合させて処理するアイテムだけのリストを作る
+    const targetDecomojiList = allDecomojiList.map((category) =>
+      category.filter(
+        (candidate) => !emojiAdminNameList.has(candidate.split(".")[0])
+      )
+    );
+    // program.debug &&
+    //   console.log(
+    //     "targetDecomojiList:",
+    //     targetDecomojiList.length,
+    //     targetDecomojiList
+    //   );
+
+    // ページに form 要素を挿入する
+    await page.evaluate(injectUploadForm);
+
+    let ratelimited = false;
+    for(let i=0; i<targetDecomojiList.length; i++) {
+      const targetAsCategory = targetDecomojiList[i];
+      const amountAsCategory = targetAsCategory.length;
+      const targetCategoryName = inputs.categories[i];
   
-      if (!result.ok) {
-        console.log(`${j + 1}/${amountAsCategory}: ${result.error} ${targetBasename}.`);
-        // ratelimited が返ってきていたら、インデックスをインクリメントせず3秒待ってもう一度実行する
-        if (result.error === "ratelimited") {
-          console.log("waiting...");
-          await page.waitFor(3000);
+      console.log(`\n[${targetCategoryName}] category start!`)
+  
+      let j = 0;
+      while(j < amountAsCategory) {
+        const item = targetAsCategory[j];
+        const targetBasename = item.split(".")[0];
+    
+        console.log(`${j + 1}/${amountAsCategory}: importing ${targetBasename}...`);
+    
+        const result = await postEmojiAdd(page, inputs.team_name, targetCategoryName, targetBasename, item);
+    
+        if (!result.ok) {
+          console.log(`${j + 1}/${amountAsCategory}: ${result.error} ${targetBasename}.`);
+          // ratelimited が返ってきたらループを終了する
+          if (result.error === "ratelimited") {
+            ratelimited = true;
+            break;
+          }
         }
-        continue;
+        // インデックスを進める
+        j++;
       }
-  
-      console.log(`${j + 1}/${amountAsCategory}: imported ${targetBasename}.`);
-      j++;
+      // ratelimited ならループを終了する
+      if (ratelimited) {
+        break;
+      }
     }
-  }
-  return;
+
+    // ブラウザを閉じる
+    if (!program.debug) {
+      await browser.close();
+    }
+
+    // ratelimited でループを抜けていたらもう一度ログイン
+    if (ratelimited) {
+      await _import(inputs, program);
+    }
+    return;
+  };
+
+  // 再帰処理をスタートする
+  await _import(inputs, program);
 };
 
 module.exports = importer;
