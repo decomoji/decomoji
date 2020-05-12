c23dfb43310ce3cf82401473eeb2174abd930524
oti <otiext@gmail.com>
2020-05-12 19:00:08 +0900

    feat(main): inputs と program をマージするようにした

--- scripts/manager/modules/importer.js
+++ scripts/manager/modules/importer.js
@@ -6,11 +6,11 @@ const goToEmojiPage = require("./goToEmojiPage");
 const injectUploadForm = require("./injectUploadForm");
 const postEmojiAdd = require("./postEmojiAdd");
 
-const importer = async (inputs, program) => {
+const importer = async (inputs) => {
 
-  const _import = async(inputs, program) => {
+  const _import = async(inputs) => {
     // puppeteer でブラウザを起動する
-    const browser = await puppeteer.launch({ devtools: program.debug });
+    const browser = await puppeteer.launch({ devtools: inputs.debug });
     // ページを追加する
     const page = await browser.newPage();
   
@@ -19,24 +19,24 @@ const importer = async (inputs, program) => {
     );
   
     // カスタム絵文字管理画面へ遷移する
-    inputs = await goToEmojiPage(page, inputs, program);
+    inputs = await goToEmojiPage(page, inputs);
 
     // 登録済みのカスタム絵文字リストを取得する
     const emojiAdminList = await page.evaluate(
       getEmojiAdminList,
       inputs.team_name
     );
-    // program.debug &&
+    // inputs.debug &&
     //   console.log("emojiAdminList:", emojiAdminList.length, emojiAdminList);
 
     // 対象デコモジリストを取得する
     const allDecomojiList = await getTargetDecomojiList(inputs.categories);
-    // program.debug &&
+    // inputs.debug &&
     //   console.log("allDecomojiList:", allDecomojiList.length, allDecomojiList);
 
     // emojiAdminList からファイル名だけの配列を作っておく
     const emojiAdminNameList = new Set(emojiAdminList.map((v) => v.name));
-    // program.debug && console.log("emojiAdminNameList:", emojiAdminNameList); 
+    // inputs.debug && console.log("emojiAdminNameList:", emojiAdminNameList); 
 
     // emojiAdminList と allDecomojiList を突合させて処理するアイテムだけのリストを作る
     const targetDecomojiList = allDecomojiList.map((category) =>
@@ -44,7 +44,7 @@ const importer = async (inputs, program) => {
         (candidate) => !emojiAdminNameList.has(candidate.split(".")[0])
       )
     );
-    // program.debug &&
+    // inputs.debug &&
     //   console.log(
     //     "targetDecomojiList:",
     //     targetDecomojiList.length,
@@ -89,19 +89,19 @@ const importer = async (inputs, program) => {
     }
 
     // ブラウザを閉じる
-    if (!program.debug) {
+    if (!inputs.debug) {
       await browser.close();
     }
 
     // ratelimited でループを抜けていたらもう一度ログイン
     if (ratelimited) {
-      await _import(inputs, program);
+      await _import(inputs);
     }
     return;
   };
 
   // 再帰処理をスタートする
-  await _import(inputs, program);
+  await _import(inputs);
 };
 
 module.exports = importer;
