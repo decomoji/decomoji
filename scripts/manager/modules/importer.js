234f22a8a915745658ea26f4652b37a5caf38307
geckotang <macky1207@gmail.com>
2020-05-12 19:31:49 +0900

    feat(importer): 2重ループをやめる

--- scripts/manager/modules/importer.js
+++ scripts/manager/modules/importer.js
@@ -4,36 +4,38 @@ const injectUploadForm = require("./injectUploadForm");
 const importer = async (page, inputs, targets) => {
   await page.evaluate(injectUploadForm);
 
-  for(let i=0; i<targets.length; i++) {
-    const targetAsCategory = targets[i];
-    const amountAsCategory = targetAsCategory.length;
-    const targetCategoryName = inputs.categories[i];
+  const targetLength = targets.length;
+  let currentCategory = '';
+  let i = 0;
 
-    console.log(`\n[${targetCategoryName}] category start!`)
+  while(i < targetLength) {
+    const target = targets[i];
+    const { category, name, path } = target;
+    const currentIdx = i + 1;
 
-    let j = 0;
-    while(j < amountAsCategory) {
-      const item = targetAsCategory[j];
-      const targetBasename = item.split(".")[0];
-  
-      console.log(`${j + 1}/${amountAsCategory}: importing ${targetBasename}...`);
-  
-      const result = await postEmojiAdd(page, inputs.team_name, targetCategoryName, targetBasename, item);
-  
-      if (!result.ok) {
-        console.log(`${j + 1}/${amountAsCategory}: ${result.error} ${targetBasename}.`);
-        // ratelimited が返ってきていたら、インデックスをインクリメントせず3秒待ってもう一度実行する
-        if (result.error === "ratelimited") {
-          console.log("waiting...");
-          await page.waitFor(3000);
-        }
-        continue;
-      }
-  
-      console.log(`${j + 1}/${amountAsCategory}: imported ${targetBasename}.`);
-      j++;
+    if (currentCategory === '' && currentCategory !== category) {
+      console.log(`\n[${category}] category start!`)
+      currentCategory = category;
     }
+
+    console.log(`${currentIdx}/${targetLength}: importing ${name}...`);
+
+    const result = await postEmojiAdd(page, inputs.team_name, name, path);
+
+    if (!result.ok) {
+      console.log(`${currentIdx}/${targetLength}: ${result.error} ${name}.`);
+      // ratelimited が返ってきていたら、インデックスをインクリメントせず3秒待ってもう一度実行する
+      if (result.error === "ratelimited") {
+        console.log("waiting...");
+        await page.waitFor(3000);
+      }
+      continue;
+    }
+
+    console.log(`${currentIdx}/${targetLength}: imported ${name}.`);
+    i++;
   }
+
   return;
 };
 
