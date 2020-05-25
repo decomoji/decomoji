8c397d568ff97e8c5bc87e11a47c6243afbb5ad2
oti <otiext@gmail.com>
2020-05-25 10:29:00 +0900

    feat(generator): デコモジ追加用の json を生成する toNamePathList.js を実装した

---
+++ scripts/generator/toNamePathList.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const toBasename = require("../manager/utilities/toBasename");
+
+const toNamePathList = (type, output) => {
+  const dir = `./decomoji/${type}/`;
+  const list = fs.readdirSync(dir).filter((v) => {
+    return /.+\.(png|gif|jpg|jpeg)$/.test(v);
+  });
+
+  const namePathList = list.map((file) => {
+    const name = toBasename(file);
+    const path = `${dir}${file}`;
+    return {
+      path,
+      name,
+    };
+  });
+
+  fs.writeFileSync(
+    `${listDir}/${output}`,
+    JSON.stringify(namePathList),
+    (err) => {
+      if (err) throw err;
+      console.log(`${output} has been saved!`);
+    }
+  );
+};
+
+toNamePathList("basic", "./scripts/manager/configs/list/v4_basic.json");
+toNamePathList("extra", "./scripts/manager/configs/list/v4_extra.json");
