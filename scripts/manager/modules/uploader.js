655b778a63d214337d407a8fd58eb38dd9d86932
Daisuke Tsuji <dim0627@gmail.com>
2020-06-23 14:12:57 +0900

    uploader.jsのerror_name_taken判定からokの場合を除外

--- scripts/manager/modules/uploader.js
+++ scripts/manager/modules/uploader.js
@@ -51,8 +51,8 @@ const uploader = async (inputs) => {
         } ${name}.`
       );
 
-      // error_name_taken 以外のエラーがあればループを抜ける
-      if (result.error !== "error_name_taken") {
+      // resultがok以外かつerror_name_taken 以外のエラーがあればループを抜ける
+      if (!result.ok && result.error !== "error_name_taken") {
         // ratelimited の場合、2FAを利用しているなら3秒待って再開、そうでなければ再ログインのためのフラグを立てる
         if (result.error === "ratelimited") {
           if (inputs.twofactor_code) {
