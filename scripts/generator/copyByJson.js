93f5c6567644ae2e924ad3cb716d71aa90c4d0a2
oti <otiext@gmail.com>
2020-07-09 17:56:59 +0900

    feat(generator): copyByJson.js にファイル移動機能を実装した

--- scripts/generator/copyByJson.js
+++ scripts/generator/copyByJson.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const copyByJson = (jsonPath, destDir) => {
+const copyByJson = (jsonPath, destDir, mode) => {
   const fileList = JSON.parse(fs.readFileSync(jsonPath));
 
   if (!fs.existsSync(destDir)) fs.mkdirSync(destDir);
@@ -8,7 +8,11 @@ const copyByJson = (jsonPath, destDir) => {
   fileList.forEach(({ path, name }) => {
     const destPath = `${destDir}${name}.png`;
     try {
-      fs.copyFileSync(path, destPath);
+      if (mode === "move") {
+        fs.renameSync(path, destPath);
+      } else {
+        fs.copyFileSync(path, destPath);
+      }
       console.log(`${path} has been copied!`);
     } catch (err) {
       throw err;
@@ -16,5 +20,6 @@ const copyByJson = (jsonPath, destDir) => {
   });
 };
 
-// node scripts/generator/copyByJson.js ~/Desktop/candidate_basic.json ~/Desktop/candidate_basic/
-copyByJson(process.argv[2], process.argv[3]);
+// node scripts/generator/copyByJson.js ~/Downloads/my-collection.json ./decomoji/my-collection/
+// node scripts/generator/copyByJson.js ~/Downloads/my-collection.json ./decomoji/my-collection/ move
+copyByJson(process.argv[2], process.argv[3], process.argv[4]);
