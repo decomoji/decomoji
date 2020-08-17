a450377040aa9bc36c9050779c0b3763f3546c68
oti <otiext@gmail.com>
2020-08-18 00:48:31 +0900

    feat(manager): getLocalJson.js で mode によって切り替えていたディレクトリ設定を削ぎ落とした

--- scripts/manager/modules/getLocalJson.js
+++ scripts/manager/modules/getLocalJson.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
 const configsDir = "./scripts/manager/configs/";
 
-const getLocalJson = (targets, mode, LOG) => {
-  const opt = mode === "alias" ? "alias/" : "list/";
+const getLocalJson = (targets, LOG) => {
   const localJson = targets
     .map((target) => {
-      return JSON.parse(
-        fs.readFileSync(`${configsDir}${opt}${target}.json`, "utf8")
-      );
+      return JSON.parse(fs.readFileSync(`${configsDir}${target}.json`, "utf8"));
     })
     .flat();
   LOG && console.log("localJson:", localJson, localJson.length);
