18a3380c648dc0307d63045c047f838310e711eb
oti <otiext@gmail.com>
2020-08-24 22:12:28 +0900

    feat(utilities): 差分をカテゴリーごとに再分配して返す getDecomojiDiffAsCategory.js を追加した

---
+++ scripts/utilities/getDecomojiDiffAsCategory.js
@@ -0,0 +1,35 @@
+const getDecomojiCategory = require("./getDecomojiCategory");
+
+// `getDecomojiDiffAsFilterMode()` の結果からバージョンを統合して { basic, extra, explicit } に再分配したオブジェクトを返す
+const getDecomojiDiffAsCategory = (diffAsMode) => {
+  const categories = {
+    basic: [],
+    extra: [],
+    explicit: [],
+  };
+  Object.entries(diffAsMode).forEach((entry) => {
+    const [filterMode, list] = entry;
+    list.forEach((decomoji) => {
+      if (filterMode === "rename") {
+        return;
+      }
+      const categoryName = getDecomojiCategory(decomoji.path);
+      const category = categories[categoryName];
+      const indexOfCategory = category.findIndex(
+        (v) => v.name === decomoji.name
+      );
+      // 同じ名前があったらマージして置き換える
+      if (indexOfCategory > -1) {
+        category.splice(indexOfCategory, 1, {
+          ...category[indexOfCategory],
+          ...decomoji,
+        });
+      } else {
+        category.push(decomoji);
+      }
+    });
+  });
+  return categories;
+};
+
+module.exports = getDecomojiDiffAsCategory;
