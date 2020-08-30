232e45458609144d850f7dcba4c947b18e83da69
oti <otiext@gmail.com>
2020-08-31 08:38:39 +0900

    feat(utilities): getGitDiffArray.js で to のバージョンが存在しない時に from だけで差分を取るように変更した

--- scripts/utilities/getGitDiffArray.js
+++ scripts/utilities/getGitDiffArray.js
@@ -1,11 +1,14 @@
 const { execSync } = require("child_process");
 const convertBufferToArray = require("./convertBufferToArray");
+const getGitTagArray = require("./getGitTagArray");
 
-// git で from と to の差分を mode で diff-filter した結果を配列を返す
+// git で from と to の差分を mode で diff-filter した結果を配列を返す。
+// to がない場合、from だけで差分を取得して返す。
 const getGitDiffArray = (from, to, mode) => {
-  const resultBuffer = execSync(
-    `git diff ${from}...${to} --name-only --diff-filter=${mode}`
-  );
+  const cmd = getGitTagArray().includes(to)
+    ? `git diff ${from}...${to} --name-only --diff-filter=${mode}`
+    : `git diff ${from} --name-only --diff-filter=${mode}`;
+  const resultBuffer = execSync(cmd);
   if (!resultBuffer) return;
   return convertBufferToArray(resultBuffer);
 };
