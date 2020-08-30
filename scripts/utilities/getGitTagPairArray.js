17f74fb7524d103b58a64af645609453a253aef8
oti <otiext@gmail.com>
2020-08-30 18:26:30 +0900

    fix(utilities): getGitTagPairArray.js で end version を最後に付与するようにした

--- scripts/utilities/getGitTagPairArray.js
+++ scripts/utilities/getGitTagPairArray.js
@@ -1,14 +1,13 @@
 const getGitTagArray = require("./getGitTagArray");
 
 // git のタグ一覧において自身と次のバージョンのペアオブジェクトを配列で返す
-const getGitTagPairArray = (versionPrefix, start) => {
+const getGitTagPairArray = (start, versionPrefix, end) => {
   const tagArray = getGitTagArray(versionPrefix);
   const tags = start ? [start, ...tagArray] : tagArray;
-  return tags.reduce((memo, cr, i, versions) => {
+  return tags.reduce((memo, from, i, versions) => {
     const next = versions[i + 1];
-    const from = cr;
-    const to = next;
-    return next ? [...memo, { from, to }] : memo;
+    const to = next ? next : end;
+    return to ? [...memo, { from, to }] : memo;
   }, []);
 };
 
