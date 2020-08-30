98d5df888ab1e6376818ac1fc698cad0159b91ff
oti <otiext@gmail.com>
2020-08-31 08:39:35 +0900

    feat(utilities): getGitDiffOfRenameArray.js で to のバージョンが存在しない時に from だけで差分を取るように変更した

--- scripts/utilities/getGitDiffOfRenameArray.js
+++ scripts/utilities/getGitDiffOfRenameArray.js
@@ -1,11 +1,14 @@
 const { execSync } = require("child_process");
 const convertBufferToArray = require("./convertBufferToArray");
+const getGitTagArray = require("./getGitTagArray");
 
 // git で rename したものの from と to の差分を返す
+// to がない場合、from だけで差分を取得して返す。
 const getGitDiffOfRenameArray = (from, to) => {
-  const resultBuffer = execSync(
-    `git diff ${from}...${to} --name-status --diff-filter=R`
-  );
+  const cmd = getGitTagArray().includes(to)
+    ? `git diff ${from}...${to} --name-status --diff-filter=R`
+    : `git diff ${from} --name-status --diff-filter=R`;
+  const resultBuffer = execSync(cmd);
   if (!resultBuffer) return;
   return convertBufferToArray(resultBuffer).map((v) =>
     v.replace("R100\t", "").split("\t")
