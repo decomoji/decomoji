622e59fc818250141aef9b35925b354e9961ce36
oti <otiext@gmail.com>
2020-09-11 15:47:10 +0900

    feat(generator): csv をオブジェクトに変換する処理を追加した

--- scripts/generator/toDuplicateJson.js
+++ scripts/generator/toDuplicateJson.js
@@ -1,5 +1,6 @@
 // node scripts/generator/toDuplicateJson.js ~/Downloads/candidate.csv
 const fs = require("fs");
+const isStringOfNotEmpty = require("../utilities/isStringOfNotEmpty");
 
 // v5_all のデータを準備しておく
 const all_buffer = fs.readFileSync("./configs/v5_all.json");
@@ -15,3 +16,20 @@ if (!CSV_PATH) {
 
 // CSV をパースして行ごとの配列にする
 const csv = fs.readFileSync(CSV_PATH).toString().split("\n");
+
+// 行を配列にしてキーと値群に分ける
+const [header, ...values] = csv.map((v) => v.replace("\r", "").split(","));
+
+// header[i] をキーにしたオブジェクトに変形する
+const labelled = values.map((row) => {
+  return row.reduce((prev, value, i) => {
+    // 値が空文字列ではない場合飲みメンバーに追加する
+    if (isStringOfNotEmpty(value)) {
+      prev[header[i]] = value;
+    }
+    return prev;
+  }, {});
+});
+
+// `ignore: TRUE` を取り除く
+const candidates = labelled.filter((v) => !v.ignore);
