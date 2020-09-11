cd626f99b4c6b21df533f8f47893329445420df0
oti <otiext@gmail.com>
2020-09-11 15:46:18 +0900

    feat(generator): コマンド引数から csv ファイルをパースする処理を追加した

--- scripts/generator/toDuplicateJson.js
+++ scripts/generator/toDuplicateJson.js
@@ -4,3 +4,14 @@ const fs = require("fs");
 // v5_all のデータを準備しておく
 const all_buffer = fs.readFileSync("./configs/v5_all.json");
 const exists = JSON.parse(all_buffer);
+
+// csv ファイルのパスは node コマンド引数から受け取る
+const CSV_PATH = process.argv[2];
+
+// 引数がなかったらエラーを投げる
+if (!CSV_PATH) {
+  throw new Error("CSV_PATH argument is undefined.");
+}
+
+// CSV をパースして行ごとの配列にする
+const csv = fs.readFileSync(CSV_PATH).toString().split("\n");
