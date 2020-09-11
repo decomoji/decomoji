d01b67db584ce020e1cbcc9174319873fa9719ad
oti <otiext@gmail.com>
2020-09-11 15:44:53 +0900

    feat(generator): v5_all.json を require した

--- scripts/generator/toDuplicateJson.js
+++ scripts/generator/toDuplicateJson.js
@@ -1,2 +1,6 @@
 // node scripts/generator/toDuplicateJson.js ~/Downloads/candidate.csv
 const fs = require("fs");
+
+// v5_all のデータを準備しておく
+const all_buffer = fs.readFileSync("./configs/v5_all.json");
+const exists = JSON.parse(all_buffer);
