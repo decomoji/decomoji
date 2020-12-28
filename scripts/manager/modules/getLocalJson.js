94dc467974dadc4ece66dd3dcadbcaa7afd64265
oti <otiext@gmail.com>
2020-12-28 19:38:11 +0900

    feat(manager): getLocalJson.js で二次元配列の targets を受け取った時も `flapMap()` で一次元の配列にして返すようにした

--- scripts/manager/modules/getLocalJson.js
+++ scripts/manager/modules/getLocalJson.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 const configsDir = "./configs/";
 
-const getLocalJson = (targets, LOG) => {
-  const localJson = targets
-    .map((target) => {
-      return JSON.parse(fs.readFileSync(`${configsDir}${target}.json`, "utf8"));
-    })
-    .flat();
-  LOG && console.log("localJson:", localJson, localJson.length);
-  return localJson;
+const getLocalJson = (targets, TERM, KEY, LOG) => {
+  // カテゴリータームでは [tag1, tag2]、バージョンタームでは [[tag1, tag2], [tag3, tag4]] で渡ってくるのでどちらとも構わず平たくする
+  const json = targets
+    .flat()
+    .flatMap((target) =>
+      JSON.parse(fs.readFileSync(`${configsDir}${target}.json`, "utf8"))
+    );
+  const formattedJson =
+    TERM === "category" ? json : json.flatMap((item) => item[KEY]);
+  LOG && console.log("formattedJson:", formattedJson, formattedJson.length);
+  return formattedJson;
 };
 module.exports = getLocalJson;
