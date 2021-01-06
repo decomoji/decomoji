f1fcadabf61c7717e7581d4a4b37ca397354034c
oti <otiext@gmail.com>
2021-01-06 22:32:08 +0900

    fix(models): 固定値で追加したり除外するアイテムを constants.js に定義した

---
+++ scripts/models/constants.js
@@ -0,0 +1,26 @@
+module.exports.ADDITIONALS = {
+  extra: [
+    {
+      name: "joinsiyo",
+      path: "decomoji/extra/joinsiyo.png",
+      created: "v5.0.0",
+      updated: "v5.14.1",
+    },
+    {
+      name: "jojo",
+      path: "decomoji/extra/jojo.png",
+      created: "v5.15.0",
+    },
+  ],
+  rename: [
+    {
+      name: "euc_jp",
+      alias_for: "euc-jp",
+    },
+  ],
+};
+
+module.exports.IGNORES = {
+  extra: ["nasca\\343\\201\\247", "true false"],
+  rename: ["nasca\\343\\201\\247", "joinsiyo", "zyoinsiyo", "true false"],
+};
