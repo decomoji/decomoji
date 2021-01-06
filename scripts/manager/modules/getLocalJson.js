5e66b94ee3dbe353e08d728361894f84ad3bbeee
oti <otiext@gmail.com>
2021-01-07 00:51:34 +0900

    refactor(manager): getLocalJson.js で変数名などを変更した

--- scripts/manager/modules/getLocalJson.js
+++ scripts/manager/modules/getLocalJson.js
@@ -3,16 +3,16 @@ const configsDir = "./configs/";
 
 const getLocalJson = (targets, TERM, KEYS, LOG) => {
   // カテゴリータームでは [tag1, tag2]、バージョンタームでは [[tag1, tag2], [tag3, tag4]] で渡ってくるのでどちらとも構わず平たくする
-  const json = targets
+  const combined = targets
     .flat()
     .flatMap((target) =>
       JSON.parse(fs.readFileSync(`${configsDir}${target}.json`, "utf8"))
     );
-  const formattedJson =
-    TERM === "category"
-      ? json
-      : KEYS.flatMap((key) => json.flatMap((item) => item[key]));
+  const filtered =
+    TERM === "version"
+      ? KEYS.flatMap((key) => combined.flatMap((item) => item[key]))
+      : combined;
   LOG && console.log("formattedJson:", formattedJson, formattedJson.length);
-  return formattedJson;
+  return filtered;
 };
 module.exports = getLocalJson;
