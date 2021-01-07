6b8eca2d9e9c401bac70d16d8d72ec265d7a5b07
oti <otiext@gmail.com>
2021-01-07 14:04:30 +0900

    refactor(utilities): getLocalJson.js のログ出力をリファクタした

--- scripts/manager/modules/getLocalJson.js
+++ scripts/manager/modules/getLocalJson.js
@@ -1,21 +1,26 @@
 const fs = require("fs");
-const configsDir = "./configs/";
+
 const writeJsonFileSync = require("../../utilities/writeJsonFileSync");
 
-const getLocalJson = (targets, TERM, KEYS, INVOKER, LOG) => {
-  LOG && console.log("getLocalJson(", { targets, TERM, INVOKER, KEYS }, ")");
-  // カテゴリータームでは [tag1, tag2]、バージョンタームでは [[tag1, tag2], [tag3, tag4]] で渡ってくるのでどちらとも構わず平たくする
-  const combined = targets
-    .flat()
-    .flatMap((target) =>
-      JSON.parse(fs.readFileSync(`${configsDir}${target}.json`, "utf8"))
-    );
-  LOG && writeJsonFileSync(combined, `./configs/_${INVOKER}_tmp_combined.json`);
+const outputLogs = (data, name, INVOKER) => {
+  writeJsonFileSync(data, `./configs/_${INVOKER}_tmp_${name}.json`);
+  console.log(`_${INVOKER}_tmp_${name}.json length:`, data.length);
+};
+
+const getLocalJson = (CONFIGS, TERM, KEYS, INVOKER, LOG) => {
+  LOG && console.log("getLocalJson(", { CONFIGS, TERM, INVOKER, KEYS }, ")");
+  // term=category では [tag1, tag2]、term=version では [[tag1, tag2], [tag3, tag4]] で渡ってくるのでどちらとも構わず平たくする
+  const combined = CONFIGS.flat().flatMap((target) =>
+    JSON.parse(fs.readFileSync(`./configs/${target}.json`, "utf8"))
+  );
+  LOG && outputLogs(combined, "combined", INVOKER);
+  // term-version の時、[{ "fixed": [...], "upload": [...], "rename": [...] }, { "fixed": [...], "upload": [...], "rename": [...] }, ...] という、
+  // filterMode 別のオブジェクトが v5.x.y ごとに配列になっているので、これから KEYS のプロパティだけ抽出して平たくする
   const filtered =
     TERM === "version"
       ? KEYS.flatMap((key) => combined.flatMap((item) => item[key]))
       : combined;
-  LOG && writeJsonFileSync(filtered, `./configs/_${INVOKER}_tmp_flatten.json`);
+  LOG && outputLogs(filtered, "filtered", INVOKER);
   return filtered;
 };
 module.exports = getLocalJson;
