e7c1c54b0dc0ebfe618fd6c3c33e8efe92f0f3e7
oti <otiext@gmail.com>
2021-01-07 14:02:20 +0900

    refacotr(utilities): writeJsonFileSync.js で書き出す JSON を整形するように変更した

--- scripts/utilities/writeJsonFileSync.js
+++ scripts/utilities/writeJsonFileSync.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // fs.writeFileSync() を try catch する
 const writeJsonFileSync = (buffer, filepath, silent) => {
   try {
-    fs.writeFileSync(`${filepath}`, JSON.stringify(buffer));
+    fs.writeFileSync(`${filepath}`, JSON.stringify(buffer, null, 2));
     !silent && console.log(`${filepath} has been saved!`);
   } catch (err) {
     throw err;
