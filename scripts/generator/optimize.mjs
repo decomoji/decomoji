3365cf075f44211f936758bb427394fb791f6fce
oti <otiext@gmail.com>
2021-07-26 13:38:12 +0900

    feat(generator): optimize.mjs の引数の処理を修正した

--- scripts/generator/optimize.mjs
+++ scripts/generator/optimize.mjs
@@ -4,14 +4,14 @@ import imagemin from "imagemin";
 import imageminPngquant from "imagemin-pngquant";
 import imageminOptipng from "imagemin-optipng";
 
-async function optimize(category) {
-  console.log(`./decomoji/${category}_tmp/**.png is optimizing...`);
-  await imagemin([`./decomoji/${category}_tmp`], {
-    destination: `./decomoji/${category}/`,
+async function optimize(arg) {
+  const path = arg === "docs" ? "docs/images" : `decomoji/${arg}`;
+  console.log(`./${path}_tmp/** is optimizing...`);
+  await imagemin([`./${path}_tmp`], {
+    destination: `./${path}/`,
     plugins: [imageminPngquant(), imageminOptipng()],
   });
-
-  console.log(`./decomoji/${category}/**.png has been optimized!`);
+  console.log(`./${path}/** has been optimized!`);
 }
 
 optimize(process.argv[2]);
