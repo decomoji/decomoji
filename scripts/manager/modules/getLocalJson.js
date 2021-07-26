35f5e3e4772fa266fd443d008b097cf393bceff3
oti <otiext@gmail.com>
2021-07-26 22:38:13 +0900

    feat(manager): getLocalJson.js を outputLogJson.js 利用に変更した

--- scripts/manager/modules/getLocalJson.js
+++ scripts/manager/modules/getLocalJson.js
@@ -1,11 +1,6 @@
 const fs = require("fs");
 
-const writeJsonFileSync = require("../../utilities/writeJsonFileSync");
-
-const outputLogs = (data, name, INVOKER) => {
-  writeJsonFileSync(data, `./configs/_${INVOKER}_tmp_${name}.json`);
-  console.log(`_${INVOKER}_tmp_${name}.json length:`, data.length);
-};
+const outputLogJson = require("../../utilities/outputLogJson");
 
 const getLocalJson = (CONFIGS, TERM, KEYS, INVOKER, LOG) => {
   LOG && console.log("getLocalJson(", { CONFIGS, TERM, INVOKER, KEYS }, ")");
@@ -13,14 +8,14 @@ const getLocalJson = (CONFIGS, TERM, KEYS, INVOKER, LOG) => {
   const combined = CONFIGS.flat().flatMap((target) =>
     JSON.parse(fs.readFileSync(`./configs/${target}.json`, "utf8"))
   );
-  LOG && outputLogs(combined, "combined", INVOKER);
+  LOG && outputLogJson(combined, "combined", INVOKER);
   // term=version の時、[{ "fixed": [...], "upload": [...], "rename": [...] }, { "fixed": [...], "upload": [...], "rename": [...] }, ...] という、
   // filterMode 別のオブジェクトが v5.x.y ごとに配列になっているので、これから KEYS のプロパティだけ抽出して平たくする
-  const filtered =
+  const flatten =
     TERM === "version"
       ? KEYS.flatMap((key) => combined.flatMap((item) => item[key]))
       : combined;
-  LOG && outputLogs(filtered, "filtered", INVOKER);
-  return filtered;
+  LOG && outputLogJson(flatten, "flatten", INVOKER);
+  return flatten;
 };
 module.exports = getLocalJson;
