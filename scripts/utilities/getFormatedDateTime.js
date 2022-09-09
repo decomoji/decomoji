1473315c1c1d4f86a95079019416d91330885a62
oti <otiext@gmail.com>
2022-09-09 18:17:22 +0900

    feat(manager): フォーマットされた現在時刻を返す `getFormatedDateTime()` を追加した

---
+++ scripts/utilities/getFormatedDateTime.js
@@ -0,0 +1,12 @@
+const getFormatedDateTime = (date) => {
+  date = date ?? new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  return `${year}_${month}_${day}_${hour}_${minute}_${second}`;
+};
+
+module.exports = getFormatedDateTime;
