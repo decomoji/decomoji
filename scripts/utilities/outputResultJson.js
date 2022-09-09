87bd491c2f03a94adb464a43f2658ffcd26ecb49
oti <otiext@gmail.com>
2022-09-09 18:20:39 +0900

    feat(manager): utilities/outputResultJson.js, ファイル名に `getFormatedDateTime()` を付与した

--- scripts/utilities/outputResultJson.js
+++ scripts/utilities/outputResultJson.js
@@ -1,7 +1,11 @@
 const writeJsonFileSync = require("./writeJsonFileSync");
+const getFormatedDateTime = require("./getFormatedDateTime");
 
 const outputResultJson = (data, name, INVOKER) => {
-  writeJsonFileSync(data, `./configs/_${INVOKER}_tmp_${name}.json`);
+  writeJsonFileSync(
+    data,
+    `./configs/_${INVOKER}_tmp_${name}_${getFormatedDateTime()}.json`
+  );
   Object.keys(data).forEach((key) => {
     console.log(`${key}: ${data[key].length}`);
   });
