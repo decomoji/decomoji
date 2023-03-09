a71a19b01b40047eaf8ee3339d555085622395b5
oti <otiext@gmail.com>
2023-03-09 14:21:59 +0900

    refactor(script): utilities/writeJsonFileSync.js, 中間物 JSON のアイテム数を出力する処理を調整した

--- scripts/utilities/outputLogJson.js
+++ scripts/utilities/outputLogJson.js
@@ -4,7 +4,6 @@ const getFormatedDateTime = require("./getFormatedDateTime");
 const outputLogJson = (data, name, INVOKER) => {
   const filename = `_${INVOKER}_tmp_${name}_${getFormatedDateTime()}.json`;
   writeJsonFileSync(data, `./configs/${filename}`);
-  console.log(`${filename} length:`, data.length);
 };
 
 module.exports = outputLogJson;
--- scripts/utilities/writeJsonFileSync.js
+++ scripts/utilities/writeJsonFileSync.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 // fs.writeFileSync() を try catch する
 const writeJsonFileSync = (buffer, filepath, silent) => {
   try {
-    fs.writeFileSync(`${filepath}`, JSON.stringify(buffer, null, 2));
-    !silent && console.log(`${filepath} has been saved!`);
+    const data = JSON.stringify(buffer, null, 2);
+    fs.writeFileSync(`${filepath}`, data);
+    !silent &&
+      console.log(`${filepath}(length: ${data.length}) has been saved!`);
   } catch (err) {
     throw err;
   }
