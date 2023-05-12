515ad579e0211e8319ef115cc3805fc1d53cfb43
oti <otiext@gmail.com>
2023-05-12 14:41:14 +0900

    refactor: scripts/utilities/convertToLowerCasedArray.mjs

--- scripts/utilities/convertToLowerCasedArray.mjs
+++ scripts/utilities/convertToLowerCasedArray.mjs
@@ -1,8 +1,5 @@
 import { isStringOfNotEmpty } from "./isStringOfNotEmpty.mjs";
 
 // 配列のアイテムを小文字に変換して返す。文字列でない場合は何もしない
-export const convertToLowerCasedArray = (array) => {
-  return array.map((item) => {
-    return isStringOfNotEmpty(item) ? item.toLowerCase() : item;
-  });
-};
+export const convertToLowerCasedArray = (array) =>
+  array.map((item) => (isStringOfNotEmpty(item) ? item.toLowerCase() : item));
