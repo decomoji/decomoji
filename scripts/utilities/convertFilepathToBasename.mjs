03d736d74041b841126cb52d6f944e3c12d9827d
oti <otiext@gmail.com>
2023-05-12 14:46:35 +0900

    refactor: scripts/utilities/convertFilepathToBasename.mjs

--- scripts/utilities/convertFilepathToBasename.mjs
+++ scripts/utilities/convertFilepathToBasename.mjs
@@ -1,6 +1,5 @@
 import path from "path";
 
 // ファイルパスから拡張子なしのファイル名を返す
-export const convertFilepathToBasename = (filepath) => {
-  return path.basename(filepath).split(".")[0];
-};
+export const convertFilepathToBasename = (filepath) =>
+  path.parse(filepath).name;
