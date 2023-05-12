ed40f8650db190ed125b9d42771ed00a33517c63
oti <otiext@gmail.com>
2023-05-12 14:15:01 +0900

    refactor: scripts/utilities/isStringOfNotEmpty.mjs

--- scripts/utilities/isStringOfNotEmpty.mjs
+++ scripts/utilities/isStringOfNotEmpty.mjs
@@ -1,7 +1,4 @@
 // 値が空ではない文字列であるか否か
-export const isStringOfNotEmpty = (value) => {
-  return (
-    Object.prototype.toString.call(value) === "[object String]" &&
-    value.length > 0
-  );
-};
+export const isStringOfNotEmpty = (value) =>
+  Object.prototype.toString.call(value) === "[object String]" &&
+  value.length > 0;
