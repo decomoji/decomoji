a38d4d9380374faf63f14d1b73cc240c07260f48
oti <otiext@gmail.com>
2023-05-12 14:40:11 +0900

    refactor: scripts/utilities/isInputs.mjs

--- scripts/utilities/isInputs.mjs
+++ scripts/utilities/isInputs.mjs
@@ -1,6 +1,5 @@
 import { isStringOfNotEmpty } from "./isStringOfNotEmpty.mjs";
 
 // 値が空ではない文字列であるか否か、否の場合エラーメッセージを返す
-export const isInputs = (value) => {
-  return isStringOfNotEmpty(value) ? true : "Input required.";
-};
+export const isInputs = (value) =>
+  isStringOfNotEmpty(value) ? true : "Input required.";
