d5440e38d9f43eca875b4e4c01f49e95571661e8
oti <otiext@gmail.com>
2023-05-12 14:40:19 +0900

    refactor: scripts/utilities/isEmail.mjs

--- scripts/utilities/isEmail.mjs
+++ scripts/utilities/isEmail.mjs
@@ -1,6 +1,5 @@
 import emailValidator from "email-validator";
 
 // 値が Valid な Email 形式であるか否か、否の場合エラーメッセージを返す
-export const isEmail = (value) => {
-  return emailValidator.validate(value) ? true : "Invalid Email format.";
-};
+export const isEmail = (value) =>
+  emailValidator.validate(value) ? true : "Invalid Email format.";
