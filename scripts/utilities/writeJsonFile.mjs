5486d73f4b4b05fcfd82b8416e6f8c3cb5825a56
oti <otiext@gmail.com>
2023-05-14 19:18:24 +0900

    refactor: scripts/utilities/writeJsonFile.mjs

--- scripts/utilities/writeJsonFile.mjs
+++ scripts/utilities/writeJsonFile.mjs
@@ -1,15 +1,14 @@
 import fs from "fs/promises";
 
 // fs.writeFileSync() を try catch する
-export const writeJsonFile = async (buffer, filepath, silent) => {
+export const writeJsonFile = async (buffer, filepath) => {
   try {
     const data = JSON.stringify(buffer, null, 2);
     await fs.writeFile(`${filepath}`, data).catch((error) => {
       console.error(error);
       throw error;
     });
-    !silent &&
-      console.log(`${filepath}(length: ${data.length}) has been saved!`);
+    console.log(`${filepath}(length: ${data.length}) has been saved!`);
   } catch (error) {
     console.error(error);
     throw error;
