7fd2223031774b6ef39808b832b9fd44f6926dad
oti <otiext@gmail.com>
2023-05-14 19:19:42 +0900

    refactor: scripts/utilities/outputLogJson.mjs

--- scripts/utilities/outputLogJson.mjs
+++ scripts/utilities/outputLogJson.mjs
@@ -2,7 +2,7 @@ import { writeJsonFile } from "./writeJsonFile.mjs";
 import { getFormatedDateTime } from "./getFormatedDateTime.mjs";
 
 export const outputLogJson = async ({ data, invoker, name }) =>
-  writeJsonFile(
+  await writeJsonFile(
     data,
-    `./configs/_tmp_${invoker}_${name}_${getFormatedDateTime()}.json`
+    `logs/_tmp_${invoker}_${name}_${getFormatedDateTime()}.json`
   );
