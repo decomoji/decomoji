59647d8a4fcfb9ad504094d62d6d462699d0864e
oti <otiext@gmail.com>
2023-12-28 22:14:07 +0900

    feat: scripts/generator/optimize.mjs, 出力ディレクトリを指定できるようにした

--- scripts/generator/optimize.mjs
+++ scripts/generator/optimize.mjs
@@ -2,14 +2,17 @@ import imagemin from "imagemin";
 import imageminPngquant from "imagemin-pngquant";
 import imageminOptipng from "imagemin-optipng";
 
-const optimize = async (arg) => {
-  const path = arg === "docs" ? "docs/images" : `decomoji/${arg}`;
+const plugins = [imageminPngquant(), imageminOptipng()];
+
+const optimize = async (category, dest) => {
+  const path = category === "docs" ? "docs/images" : `decomoji/${category}`;
+  const destination = dest ? `./${dest}/` : `./${path}/`;
   console.log(`./${path}_tmp/** is optimizing...`);
   await imagemin([`./${path}_tmp`], {
-    destination: `./${path}/`,
-    plugins: [imageminPngquant(), imageminOptipng()],
+    destination,
+    plugins,
   });
   console.log(`./${path}/** has been optimized!`);
 };
 
-optimize(process.argv[2]);
+optimize(process.argv[2], process.argv[3]);
