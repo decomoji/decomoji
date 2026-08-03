651ceb0d79b0f7645db0ecc36ec02e8e4709b42b
oti <otiext@gmail.com>
2026-08-03 10:53:42 +0900

    feat: バレルファイルを更新した

--- scripts/launcher/modules/index.mjs
+++ scripts/launcher/modules/index.mjs
@@ -1,4 +1,5 @@
 export { assigner } from "./assigner.mjs";
+export { curator } from "./curator.mjs";
 export { dialoger } from "./dialoger.mjs";
 export { getConfigJson } from "./getConfigJson.mjs";
 export { goToEmojiPage } from "./goToEmojiPage.mjs";
