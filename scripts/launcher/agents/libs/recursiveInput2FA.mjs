e24ccb634c4c455bdf923b2f0a9b6f67bc8e5e78
oti <otiext@gmail.com>
2026-08-12 22:51:17 +0900

    fix: 再起処理で意図通りに `input` を返せていなかったのを修正した

--- scripts/launcher/agents/libs/recursiveInput2FA.mjs
+++ scripts/launcher/agents/libs/recursiveInput2FA.mjs
@@ -6,28 +6,27 @@ export const recursiveInput2FA = async (browser, page, inputs) => {
   // 前の入力を空にしておく
   await page.$eval('[name="2fa_code"]', (e) => (e.value = ""));
   // 2FA試行
-  try {
-    const { twofactor_code } = await inquirer.prompt({
-      type: "password",
-      name: "twofactor_code",
-      mask: "*",
-      message: "2FA コードを入力してください:",
-      validate: isInputs,
-    });
-    // 2FA 利用のフラグを立てる
-    inputs.twofactor_code = true;
-    // フォームに入力してサインインする
-    const $2fa = await page.$(".two_factor_input_item:first-child > input");
-    await $2fa.type(twofactor_code);
-    await page.waitForNavigation({ waitUntil: "networkidle2" });
-    // 2FA入力欄がなかったら2FA認証できたと見なして再帰処理を抜ける
-    if (!(await page.$('[name="2fa_code"]'))) {
-      console.info("2FA Verified!");
-      return inputs;
-    }
-    // 2FA認証できるまで何度でもトライ！
-    await recursiveInput2FA(browser, page, inputs);
-  } catch (e) {
-    return e;
+  const { twofactor_code } = await inquirer.prompt({
+    type: "password",
+    name: "twofactor_code",
+    mask: "*",
+    message: "2FA コードを入力してください:",
+    validate: isInputs,
+  });
+  // 2FA 利用のフラグを立てる
+  inputs.twofactor_code = true;
+  // フォームに入力してサインインする
+  // 最後の桁を入力した時点で自動送信されるので、入力と遷移待ちを同時に開始する
+  const $2fa = await page.$(".two_factor_input_item:first-child > input");
+  await Promise.all([
+    $2fa.type(twofactor_code),
+    page.waitForNavigation({ waitUntil: "networkidle2" }),
+  ]);
+  // 2FA入力欄がなかったら2FA認証できたと見なして再帰処理を抜ける
+  if (!(await page.$('[name="2fa_code"]'))) {
+    console.info("2FA Verified!");
+    return inputs;
   }
+  // 2FA認証できるまで何度でもトライ！
+  return await recursiveInput2FA(browser, page, inputs);
 };
