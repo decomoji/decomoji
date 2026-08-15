e074f27fd7b7aef1b135a14fc72d2bd1c4f37d52
oti <otiext@gmail.com>
2026-08-15 22:40:32 +0900

    feat: Slackのログインページに遷移する処理を goToSignInPage.mjs に切り出した

---
+++ scripts/launcher/agents/libs/goToSignInPage.mjs
@@ -0,0 +1,17 @@
+// ログイン画面に遷移する（ログイン後にカスタム絵文字管理画面にリダイレクトさせる）
+export const goToSignInPage = async (page, workspace) => {
+  await page.goto(
+    `https://${workspace}.slack.com/sign_in_with_password?redir=%2Fcustomize%2Femoji#/`,
+    {
+      waitUntil: "domcontentloaded",
+    },
+  );
+
+  // SSOログインが有効なときはフォームがないのでパスワードログイン用の画面に遷移する
+  // パスワードログインへの導線は form のことも a のこともあるため、フォームの不在そのものを条件にする
+  if (!(await page.$("#signin_form"))) {
+    await page.goto(`https://${workspace}.slack.com/?no_sso=1&redir=%2Fcustomize%2Femoji#/`, {
+      waitUntil: "domcontentloaded",
+    });
+  }
+};
